7ef9f593ad9caa4ef1e86533844c0ad490c243bb
anydream <anydreamplay@hotmail.com>
2017-08-25 20:18:34 +0800

    Add test cases

--- test/testcase/Program.cs
+++ test/testcase/Program.cs
@@ -14,8 +14,54 @@ namespace group1
 	class Derived<T, P> : Base<P, T>
 	{
 		private T fld;
-
 		public override void Foo(P a, T b)
+		{
+			fld = b;
+		}
+	}
+}
+
+namespace group2
+{
+	interface Inf<T, P>
+	{
+		void Foo(T a, P b);
+	}
+
+	class Base<T, P> : Inf<T, P>
+	{
+		private T fld;
+		private int fldInt;
+		public virtual void Foo(T a, P b)
+		{
+			fld = a;
+		}
+
+		public virtual void Foo(int a, float b)
+		{
+			fldInt = a;
+		}
+	}
+
+	class Derived<T, P> : Base<P, T>
+	{
+		private T fld;
+		private int fldInt;
+		public override void Foo(P a, T b)
+		{
+			fld = b;
+		}
+
+		public override void Foo(int a, float b)
+		{
+			fldInt = a;
+		}
+	}
+
+	class DerivedX2<T> : Derived<T, int>
+	{
+		private T fld;
+		public override void Foo(int a, T b)
 		{
 			fld = b;
 		}
@@ -56,6 +102,18 @@ namespace testcase
 			group1.Base<int, float> b = new group1.Base<int, float>();
 			b.Foo(1, 1.2f);
 			var d = new group1.Derived<float, int>();
+		}
+	}
+
+	[Test]
+	static class GenOverride4
+	{
+		public static void Entry()
+		{
+			group2.Inf<int, float> i = new group2.DerivedX2<float>();
+			i.Foo(1, 1.2f);
+			group2.Inf<float, int> i2 = null;
+			i2.Foo(1.2f, 1);
 		}
 	}
 
