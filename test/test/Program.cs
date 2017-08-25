6b5967e33fdf8195ed219c9d3ae0179b43ad6106
anydream <anydreamplay@hotmail.com>
2017-08-25 15:31:04 +0800

    Add test project

---
+++ test/test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using il2cpp;
+
+namespace test
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Il2cppContext context = new Il2cppContext(@"testCS.exe");
+			context.AddEntry(context.Module.EntryPoint);
+			context.Process();
+
+			HierarchyDump dumper = new HierarchyDump(context);
+			StringBuilder sb = new StringBuilder();
+
+			sb.Append("* MethodTables:\n");
+			dumper.DumpMethodTables(sb);
+
+			sb.Append("* Types:\n");
+			dumper.DumpTypes(sb);
+
+			Console.Write(sb.ToString());
+		}
+	}
+}
