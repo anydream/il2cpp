a2bb8d1476e8ab85687085f0319fec4c6b08c134
anydream <anydreamplay@hotmail.com>
2017-09-25 20:42:42 +0800

    Convert source codes to UTF-8 encoding

--- runtime/il2cpp.cpp
+++ runtime/il2cpp.cpp
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+﻿#include <stdlib.h>
 
 #if defined(_WIN32)
 #include <windows.h>
--- runtime/il2cppGC.cpp
+++ runtime/il2cppGC.cpp
@@ -1,4 +1,4 @@
-#include <stdint.h>
+﻿#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <gc.h>
