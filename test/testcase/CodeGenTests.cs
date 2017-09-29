386900d35a9b82bb09c796f34e38888c2466c0e7
anydream <anydreamplay@hotmail.com>
2017-09-29 15:33:35 +0800

    Add ray trace test 2

--- test/testcase/CodeGenTests.cs
+++ test/testcase/CodeGenTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace testcase
@@ -645,7 +646,7 @@ namespace testcase
 	[CodeGen]
 	static class TestRayTrace
 	{
-#if true
+#if false
 		static extern double MathSqrt(double n);
 		static extern double MathAbs(double n);
 		static extern double MathSin(double n);
@@ -1177,17 +1178,396 @@ namespace testcase
 		}
 	}
 
+	static class TestRayTrace2
+	{
+		static double M_PI = 3.141592653589793238462643;
+
+		public class RandomLCG
+		{
+			uint mSeed;
+
+			public RandomLCG(uint seed = 0)
+			{
+				mSeed = seed;
+			}
+
+			public double NextNumber()
+			{
+				mSeed = 214013u * mSeed + 2531011u;
+				return mSeed * (1.0 / 4294967296.0);
+			}
+		};
+
+		public struct Vec
+		{
+			public double x, y, z;
+
+			public Vec(double px, double py, double pz)
+			{
+				x = px;
+				y = py;
+				z = pz;
+			}
+
+			public Vec Add(ref Vec b)
+			{
+				return new Vec(x + b.x, y + b.y, z + b.z);
+			}
+
+			public Vec Sub(ref Vec b)
+			{
+				return new Vec(x - b.x, y - b.y, z - b.z);
+			}
+
+			public Vec Mul(double b)
+			{
+				return new Vec(x * b, y * b, z * b);
+			}
+
+			public Vec Mul(ref Vec b)
+			{
+				return new Vec(x * b.x, y * b.y, z * b.z);
+			}
+
+			public Vec Norm()
+			{
+				return this.Mul(1.0 / Math.Sqrt(x * x + y * y + z * z));
+			}
+
+			public double Dot(ref Vec b)
+			{ // dot
+				return x * b.x + y * b.y + z * b.z;
+			}
+
+			public Vec Cross(ref Vec b)
+			{ // cross
+				return new Vec(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x);
+			}
+		};
+
+		static readonly Vec Vec_Zero = new Vec(0, 0, 0);
+		static readonly Vec Vec_XAxis = new Vec(1, 0, 0);
+		static readonly Vec Vec_YAxis = new Vec(0, 1, 0);
+		static readonly Vec Vec_ZAxis = new Vec(0, 0, 1);
+
+		public struct Ray
+		{
+			public Vec o, d;
+
+			public Ray(ref Vec po, ref Vec pd)
+			{
+				o = po;
+				d = pd;
+			}
+		};
+
+		// material types, used in radiance()
+		public enum MatType
+		{
+			DIFF,
+			SPEC,
+			REFR
+		};
+
+		public struct Sphere
+		{
+			public Vec p, e, c;      // position, emission, color
+			public Vec cc;
+			public double rad;       // radius
+			public double sqRad;
+			public double maxC;
+			public MatType refl;      // reflection type (DIFFuse, SPECular, REFRactive)
+
+			public Sphere(double prad, ref Vec pp, ref Vec pe, ref Vec pc, MatType prefl)
+			{
+				p = pp;
+				e = pe;
+				c = pc;
+				rad = prad;
+				refl = prefl;
+
+				sqRad = rad * rad;
+				maxC = c.x > c.y && c.y > c.z ? c.x : c.y > c.z ? c.y : c.z;
+				cc = c.Mul(1.0 / maxC);
+			}
+
+			public Sphere(double prad, Vec pp, Vec pe, Vec pc, MatType prefl)
+			{
+				p = pp;
+				e = pe;
+				c = pc;
+				rad = prad;
+				refl = prefl;
+
+				sqRad = rad * rad;
+				maxC = c.x > c.y && c.y > c.z ? c.x : c.y > c.z ? c.y : c.z;
+				cc = c.Mul(1.0 / maxC);
+			}
+
+			// returns distance, 1e20 if nohit
+			public double intersect(ref Ray r)
+			{
+				// Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
+				Vec op = p.Sub(ref r.o);
+				double b = op.Dot(ref r.d);
+				double det = b * b - op.Dot(ref op) + sqRad;
+				double eps = 1e-4;
+
+				if (det < 0)
+					return 1e20;
+				else
+				{
+					double dets = Math.Sqrt(det);
+
+					if (b - dets > eps)
+						return b - dets;
+					else if (b + dets > eps)
+						return b + dets;
+					else
+						return 1e20;
+				}
+			}
+		};
+
+		//Scene: radius, position, emission, color, material
+		static Sphere[] spheres =
+		{
+			new Sphere(1e5,  new Vec(1e5 + 1,40.8,81.6),   Vec_Zero, new Vec(.75,.25,.25), MatType.DIFF),//Left
+			new Sphere(1e5,  new Vec(-1e5 + 99,40.8,81.6), Vec_Zero, new Vec(.25,.25,.75), MatType.DIFF),//Rght
+			new Sphere(1e5,  new Vec(50,40.8, 1e5),        Vec_Zero, new Vec(.75,.75,.75), MatType.DIFF),//Back
+			new Sphere(1e5,  new Vec(50,40.8,-1e5 + 170),  Vec_Zero, Vec_Zero,             MatType.DIFF),//Frnt
+			new Sphere(1e5,  new Vec(50, 1e5, 81.6),       Vec_Zero, new Vec(.75,.75,.75), MatType.DIFF),//Botm
+			new Sphere(1e5,  new Vec(50,-1e5 + 81.6,81.6), Vec_Zero, new Vec(.75,.75,.75), MatType.DIFF),//Top
+			new Sphere(16.5, new Vec(27,16.5,47),          Vec_Zero, new Vec(1,1,1).Mul(.999),  MatType.SPEC),//Mirr
+			new Sphere(16.5, new Vec(73,16.5,78),          Vec_Zero, new Vec(1,1,1).Mul(.999),  MatType.REFR),//Glas
+			new Sphere(600,  new Vec(50,681.6 - .27,81.6), new Vec(12,12,12), Vec_Zero,         MatType.DIFF) //Lite
+		};
+
+		public static double clamp(double x)
+		{
+			if (x < 0)
+				return 0;
+			else if (x > 1)
+				return 1;
+			else
+				return x;
+		}
+
+		public static int toInt(double x)
+		{
+			return (int)(Math.Pow(clamp(x), 1 / 2.2) * 255 + .5);
+		}
+
+		public static unsafe Sphere* intersect(ref Ray r, out double t)
+		{
+			t = 1e20;
+			Sphere* ret = null;
+
+			fixed (Sphere* pStart = &spheres[0])
+			{
+				int sz = spheres.Length;
+				for (Sphere* s = pStart; s != pStart + sz; ++s)
+				{
+					double d = s->intersect(ref r);
+					if (d < t)
+					{
+						t = d;
+						ret = s;
+					}
+				}
+			}
+			return ret;
+		}
+
+		public static unsafe Vec radiance(ref Ray r, int depth, RandomLCG rand)
+		{
+			double t;                               // distance to intersection
+			Sphere* obj = intersect(ref r, out t);
+
+			if (obj == null)
+				return Vec_Zero; // if miss, return black
+			else
+			{
+				int newDepth = depth + 1;
+				bool isMaxDepth = newDepth > 100;
+
+				// Russian roulette for path termination
+				bool isUseRR = newDepth > 5;
+				bool isRR = isUseRR && rand.NextNumber() < obj->maxC;
+
+				if (isMaxDepth || (isUseRR && !isRR))
+					return obj->e;
+				else
+				{
+					Vec f = (isUseRR && isRR) ? obj->cc : obj->c;
+					var tmp1 = r.d.Mul(t);
+					Vec x = r.o.Add(ref tmp1);
+					Vec n = (x.Sub(ref obj->p)).Norm();
+					Vec nl = n.Dot(ref r.d) < 0 ? n : n.Mul(-1);
+
+					if (obj->refl == MatType.DIFF)
+					{ // Ideal DIFFUSE reflection
+						double r1 = 2 * M_PI * rand.NextNumber();
+						double r2 = rand.NextNumber();
+						double r2s = Math.Sqrt(r2);
+
+						Vec w = nl;
+						Vec wo = w.x < -0.1 || w.x > 0.1 ? Vec_YAxis : Vec_XAxis;
+						Vec u = (wo.Cross(ref w)).Norm();
+						Vec v = w.Cross(ref u);
+
+						var tmp2 = v.Mul(Math.Sin(r1)).Mul(r2s);
+						var tmp3 = w.Mul(Math.Sqrt(1 - r2));
+						Vec d = (u.Mul(Math.Cos(r1)).Mul(r2s).Add(ref tmp2).Add(ref tmp3)).Norm();
+
+						var tmp4 = new Ray(ref x, ref d);
+						var tmp5 = radiance(ref tmp4, newDepth, rand);
+						var tmp6 = f.Mul(ref tmp5);
+						return obj->e.Add(ref tmp6);
+					}
+					else if (obj->refl == MatType.SPEC) // Ideal SPECULAR reflection
+					{
+						var tmp8 = n.Mul(2 * (n.Dot(ref r.d)));
+						var tmp9 = r.d.Sub(ref tmp8);
+						var tmp7 = new Ray(ref x, ref tmp9);
+						var tmp10 = radiance(ref tmp7, newDepth, rand);
+						var tmp11 = f.Mul(ref tmp10);
+						return obj->e.Add(ref tmp11);
+					}
+					else
+					{ // Ideal dielectric REFRACTION
+						var tmp100 = n.Mul(2 * (n.Dot(ref r.d)));
+						var tmp101 = r.d.Sub(ref tmp100);
+						Ray reflRay = new Ray(ref x, ref tmp101);
+						bool into = n.Dot(ref nl) > 0;  // Ray from outside going in?
+						double nc = 1;
+						double nt = 1.5;
+						double nnt = into ? nc / nt : nt / nc;
+						double ddn = r.d.Dot(ref nl);
+						double cos2t = 1 - nnt * nnt * (1 - ddn * ddn);
+
+						if (cos2t < 0) // Total internal reflection
+						{
+							var tmp12 = radiance(ref reflRay, newDepth, rand);
+							var tmp13 = f.Mul(ref tmp12);
+							return obj->e.Add(ref tmp13);
+						}
+						else
+						{
+							var tmp14 = n.Mul((into ? 1 : -1) * (ddn * nnt + Math.Sqrt(cos2t)));
+							Vec tdir = (r.d.Mul(nnt).Sub(ref tmp14)).Norm();
+							double a = nt - nc;
+							double b = nt + nc;
+							double R0 = (a * a) / (b * b);
+							double c = 1 - (into ? -ddn : tdir.Dot(ref n));
+							double Re = R0 + (1 - R0) * c * c * c * c * c;
+							double Tr = 1 - Re;
+							double P = .25 + .5 * Re;
+							double RP = Re / P;
+							double TP = Tr / (1 - P);
+
+							Vec result;
+							if (newDepth > 2)
+							{
+								// Russian roulette and splitting for selecting reflection and/or refraction
+								if (rand.NextNumber() < P)
+									result = radiance(ref reflRay, newDepth, rand).Mul(RP);
+								else
+								{
+									var tmp15 = new Ray(ref x, ref tdir);
+									result = radiance(ref tmp15, newDepth, rand).Mul(TP);
+								}
+							}
+							else
+							{
+								var tmp16 = new Ray(ref x, ref tdir);
+								var tmp17 = radiance(ref tmp16, newDepth, rand).Mul(Tr);
+								result = radiance(ref reflRay, newDepth, rand).Mul(Re).Add(ref tmp17);
+							}
+
+							var tmp18 = f.Mul(ref result);
+							return obj->e.Add(ref tmp18);
+						}
+					}
+				}
+			}
+		}
+
+		public static Vec[] Entry()
+		{
+			int w = 256;
+			int h = 256;
+			int samps = 25; // # samples
+
+			var tmp1 = new Vec(50, 52, 295.6);
+			var tmp2 = new Vec(0, -0.042612, -1).Norm();
+			Ray cam = new Ray(ref tmp1, ref tmp2); // cam pos, dir
+			Vec cx = new Vec(w * .5135 / h, 0, 0);
+			Vec cy = (cx.Cross(ref cam.d)).Norm().Mul(.5135);
+			Vec[] c = new Vec[w * h];
+
+			//#pragma omp parallel for schedule(dynamic, 1)       // OpenMP
+			// Loop over image rows
+			for (int y = 0; y < h; y++)
+			{
+				//fprintf(stderr,"\rRendering (%d spp) %5.2f%%",samps*4,100.*y/(h-1));
+				RandomLCG rand = new RandomLCG((uint)y);
+
+				// Loop cols
+				for (ushort x = 0; x < w; x++)
+				{
+					// 2x2 subpixel rows
+					for (int sy = 0; sy < 2; sy++)
+					{
+						int i = (h - y - 1) * w + x;
+
+						// 2x2 subpixel cols
+						for (int sx = 0; sx < 2; sx++)
+						{
+							Vec r = Vec_Zero;
+							for (int s = 0; s < samps; s++)
+							{
+								double r1 = 2 * rand.NextNumber();
+								double r2 = 2 * rand.NextNumber();
+								double dx = r1 < 1 ? Math.Sqrt(r1) - 1 : 1 - Math.Sqrt(2 - r1);
+								double dy = r2 < 1 ? Math.Sqrt(r2) - 1 : 1 - Math.Sqrt(2 - r2);
+
+								var tmp3 = cy.Mul(((sy + .5 + dy) / 2 + y) / h - .5);
+								Vec d = cx.Mul(((sx + .5 + dx) / 2 + x) / w - .5).Add(
+									ref tmp3).Add(ref cam.d);
+
+								var tmp4 = d.Mul(140);
+								var tmp5 = cam.o.Add(ref tmp4);
+								var tmp6 = d.Norm();
+								var tmp7 = new Ray(ref tmp5, ref tmp6);
+								var tmp8 = radiance(ref tmp7, 0, rand).Mul(1.0 / samps);
+								r = r.Add(ref tmp8);
+							}
+							var tmp9 = new Vec(clamp(r.x), clamp(r.y), clamp(r.z)).Mul(.25);
+							c[i] = c[i].Add(ref tmp9);
+						}
+					}
+				}
+			}
+			return c;
+		}
+	}
+
 	internal class Program
 	{
 		private static void Main()
 		{
-			var c = TestRayTrace.Entry();
+			var tw = new Stopwatch();
+			tw.Start();
+			var c = TestRayTrace2.Entry();
+			tw.Stop();
+			Console.WriteLine("Elapsed: {0}", tw.ElapsedMilliseconds);
 
 			using (StreamWriter sw = new StreamWriter("imageCS.ppm"))
 			{
 				sw.Write("P3\r\n{0} {1}\r\n{2}\r\n", 256, 256, 255);
 				for (int i = 0; i < 256 * 256; i++)
-					sw.Write("{0} {1} {2}\r\n", TestRayTrace.Smallpt.toInt(c[i].x), TestRayTrace.Smallpt.toInt(c[i].y), TestRayTrace.Smallpt.toInt(c[i].z));
+					sw.Write("{0} {1} {2}\r\n", TestRayTrace2.toInt(c[i].x), TestRayTrace2.toInt(c[i].y), TestRayTrace2.toInt(c[i].z));
 			}
 
 			//Console.Write("Input Times: ");
