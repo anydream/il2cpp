c42770c260ad5630f3b3db38d7e7337f86a0084e
anydream <anydreamplay@hotmail.com>
2017-11-08 22:38:22 +0800

    ~

--- il2cpp/MethodGenerator.cs
+++ il2cpp/MethodGenerator.cs
@@ -1886,9 +1886,9 @@ namespace il2cpp
 			if (len > 1)
 			{
 				SlotInfo last = ary[len - 1];
-				for (int i = 0; i < len - 1; ++i)
+				for (int i = len - 1; i >= 1; --i)
 				{
-					ary[i + 1] = ary[i];
+					ary[i] = ary[i - 1];
 				}
 				ary[0] = last;
 				return true;
