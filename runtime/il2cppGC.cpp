8b41a086db42714d2f02eb5973818c3460b18057
anydream <anydreamplay@hotmail.com>
2017-11-28 19:57:11 +0800

    Add multi-threading GC support

--- runtime/il2cppGC.cpp
+++ runtime/il2cppGC.cpp
@@ -5,6 +5,9 @@
 void il2cpp_GC_Init()
 {
 	GC_INIT();
+#if defined(GC_THREADS)
+	GC_allow_register_threads();
+#endif
 }
 
 void* il2cpp_GC_Alloc(uintptr_t sz)
@@ -17,6 +20,23 @@ void* il2cpp_GC_AllocAtomic(uintptr_t sz)
 	void* ptr = GC_MALLOC_ATOMIC(sz);
 	memset(ptr, 0, sz);
 	return ptr;
+}
+
+bool il2cpp_GC_RegisterThread(void* basePtr)
+{
+#if defined(GC_THREADS)
+	GC_stack_base sb;
+	int res = GC_get_stack_base(&sb);
+	if (res != GC_SUCCESS)
+		sb.mem_base = basePtr;
+
+	res = GC_register_my_thread(&sb);
+	if ((res != GC_SUCCESS) && (res != GC_DUPLICATE))
+		return false;
+	return true;
+#else
+	return false;
+#endif
 }
 
 static void GC_CALLBACK FinalizerCallback(void* obj, void* cdata)
--- runtime/il2cppGC.h
+++ runtime/il2cppGC.h
@@ -3,6 +3,7 @@
 void il2cpp_GC_Init();
 void* il2cpp_GC_Alloc(uintptr_t sz);
 void* il2cpp_GC_AllocAtomic(uintptr_t sz);
+bool il2cpp_GC_RegisterThread(void* basePtr);
 void il2cpp_GC_RegisterFinalizer(cls_Object* obj, IL2CPP_FINALIZER_FUNC finalizer);
 void il2cpp_GC_Collect();
 
