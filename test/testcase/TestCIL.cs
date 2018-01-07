218cbd294faad7eb667e7bb89f3b1ecf78699620
anydream <anydreamplay@hotmail.com>
2018-01-07 21:37:13 +0800

    Converts encoding and newlines

--- test/testcase/TestCIL.cs
+++ test/testcase/TestCIL.cs
@@ -1,1 +1,15 @@
-﻿namespace testcase{	[CodeGen]	static class TestCIL	{		public static int Entry()		{			int res = InterfaceDefinition.HelloWorld.Main();			if (res != 100)				return 1;			return 0;		}	}}
+﻿namespace testcase
+{
+	[CodeGen]
+	static class TestCIL
+	{
+		public static int Entry()
+		{
+			int res = InterfaceDefinition.HelloWorld.Main();
+			if (res != 100)
+				return 1;
+
+			return 0;
+		}
+	}
+}
