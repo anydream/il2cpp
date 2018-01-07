9cb0b05959b6327e6ea910e0e28df5e609c531fc
anydream <anydreamplay@hotmail.com>
2018-01-07 21:23:26 +0800

    Add CIL InterfaceDefinition test

---
+++ test/testcase/TestCIL.cs
@@ -0,0 +1,1 @@
+﻿namespace testcase{	[CodeGen]	static class TestCIL	{		public static int Entry()		{			int res = InterfaceDefinition.HelloWorld.Main();			if (res != 100)				return 1;			return 0;		}	}}
